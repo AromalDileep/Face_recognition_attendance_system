2e85e2c1649667c2ec2f49f13ea4ea7312acd08a
aromal <aromaldileep96@gmail.com>
2026-02-23 23:22:27 +0530

    log files added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import logging
 from ui.gui import run_gui
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename='app.log', 
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
     run_gui()
